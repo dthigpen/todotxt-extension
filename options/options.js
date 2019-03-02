387760173eb17533044f21cb3eff8a3b85110764
dthigpen <davidthigs@gmail.com>
2019-03-02 16:02:36 -0700

    Switch to sync storage for multiple computer usage

--- options/options.js
+++ options/options.js
@@ -1,70 +1,65 @@
 const commandName = '_execute_browser_action';
+
+browser.storage.onChanged = updateUI;
+
+async function initOptions(force=false) {
+    let options = await browser.storage.sync.get("options");
+    if(force || typeof options === "undefined" || !options.hasOwnProperty("options")) {
+        let options = {
+            options: {
+                shortcuts: {},
+                notifications: {},
+                other: {}
+            }
+        };
+        console.log("Reseting options");
+        await browser.storage.sync.set(options);
+        await resetShortcuts();
+    }
+}
 /**
  * Update the UI: set the value of the shortcut textbox.
  */
 async function updateUI() {
     // fetch options values from storage and populate options elements
-    browser.storage.local.get("options")
-    .then(options => {
-        options = options.options;
-        if(typeof options !== undefined) {
-            let shortcuts = options.shortcuts;
-            for(var prop in shortcuts) {
-                // skip loop if the property is from prototype
-                if (!shortcuts.hasOwnProperty(prop)) continue;
-                let foundKey = shortcuts[prop].key;
-                console.log(shortcuts[prop]);
-                if(typeof foundKey === "undefined" || foundKey.length == 0) {
-                    document.querySelector("#" + prop).value = shortcuts[prop].default;
-                } else {
-                    document.querySelector("#" + prop).value = foundKey;
-                }
+    let options = await fetchOptions();
+    if(typeof options !== "undefined" && typeof options.options !== "undefined") {
+        let shortcuts = options.options.shortcuts;
+        for(var prop in shortcuts) {
+            // skip loop if the property is from prototype
+            if (!shortcuts.hasOwnProperty(prop)) continue;
+            let foundKey = shortcuts[prop].key;
+            if(typeof foundKey === "undefined" || foundKey.length === 0) {
+                document.querySelector("#" + prop).value = shortcuts[prop].default;
+            } else {
+                document.querySelector("#" + prop).value = foundKey;
             }
         }
-    })
-    .catch(err => {
-        console.error(err);
-    });
-
+    } else {
+        // reset the options structure
+        initOptions(true);
+    }
+   
     let commands = await browser.commands.getAll();
     for (command of commands) {
       if (command.name === commandName) {
         document.querySelector('#browserAction').value = command.shortcut;
       }
     }
-  }
+}
   
   /**
    * Update the shortcut based on the value in the textbox.
    */
-  async function updateShortcut() {
-    let options = {
-        options: {
-            shortcuts: {
-                browserAction: {
-                    default: "Ctrl+Shift+L",
-                    key: ""
-                },
-                keyComplete: {
-                    default: "C",
-                    key: ""
-                },
-                keyEdit: {
-                    default: "E",
-                    key: ""
-                },
-            }
-        }
-    };
+  async function updateShortcuts(e) {
+    e.preventDefault();  
+    let options = await fetchOptions();
     let shortcuts = options.options.shortcuts;
-    for(var prop in shortcuts) {
-        if (!shortcuts.hasOwnProperty(prop)) continue;
-        let inputKey = document.querySelector("#" + prop).value;
-        console.log("saving: " + inputKey);
-        shortcuts[prop].key = inputKey.length > 0 ? inputKey : shortcuts[prop].default;
+    let shortcutOptions = document.querySelectorAll(".shortcutOptions input");
+    for(let i = 0; i < shortcutOptions.length; i++) {
+        shortcuts[shortcutOptions[i].id].key = shortcutOptions[i].value;
     }
-    browser.storage.local.set(options);
-    
+    await browser.storage.sync.set(options);
     await browser.commands.update({
       name: commandName,
       shortcut: document.querySelector('#browserAction').value
@@ -74,39 +69,44 @@ async function updateUI() {
   /**
    * Reset the shortcut and update the textbox.
    */
-  async function resetShortcut() {
-    let options = {
-        options: {
-            shortcuts: {
-                browserAction: {
-                    default: "Ctrl+Shift+L",
-                    key: ""
-                },
-                keyComplete: {
-                    default: "C",
-                    key: ""
-                },
-                keyEdit: {
-                    default: "E",
-                    key: ""
-                },
-            }
-        }
+  async function resetShortcuts(e) {
+    e.preventDefault();
+    let shortcuts = {
+        browserAction: {
+            default: "Ctrl+Shift+L",
+            key: "Ctrl+Shift+L"
+        },
+        keyComplete: {
+            default: "C",
+            key: "C"
+        },
+        keyEdit: {
+            default: "E",
+            key: "E"
+        },
     };
-    browser.storage.local.set(options);
+    let options = await fetchOptions();
+    options.options.shortcuts = shortcuts;
+    await browser.storage.sync.set(options);
     await browser.commands.reset(commandName);
-    updateUI();
   }
   
   async function clearLocalTodos() {
-      await browser.storage.local.remove("items");
+      await browser.storage.sync.remove("items");
   }
 
+  async function fetchOptions() {
+      let options = await browser.storage.sync.get("options");
+      return options;
+  }
   /**
    * Update the UI when the page loads.
    */
   document.addEventListener('DOMContentLoaded', updateUI);
   
-  document.querySelector('#update').addEventListener('click', updateShortcut);
-  document.querySelector('#reset').addEventListener('click', resetShortcut);
+  document.querySelector('#update').addEventListener('click', updateShortcuts);
+  document.querySelector('#reset').addEventListener('click', resetShortcuts);
   document.querySelector('#deleteTodos').addEventListener('click', clearLocalTodos);
+  document.querySelector('#resetAll').addEventListener('click', () => initOptions(true));
+  
+  initOptions();
